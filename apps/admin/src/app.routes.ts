1a2426db23755008e819ecff11e13c5b2c4359cb
Öznur Özçelik <oznurozclk35@gmail.com>
2025-12-13 00:55:48 +0300

    Admin paneli için lazy loading routing yapısı kuruldu (Login, Layouts, Home)

--- apps/admin/src/app.routes.ts
+++ apps/admin/src/app.routes.ts
@@ -1,3 +1,19 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path:"login",
+        loadComponent: () => import("./pages/login/login")
+    },
+    {
+        path:"",
+        loadComponent:()=> import("./pages/layouts/layouts"),
+        children:[
+            {
+                path:"",
+                loadComponent: ()=> import("./pages/home/home")
+            }
+        ]
+    }
+   
+];
